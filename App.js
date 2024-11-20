9424f3b7393082973c876ed9cbe2700018f93062
GerryRguez <gfarfan_22@alu.uabcs.mx>
2024-11-20 16:14:12 -0700

    Update App.js information

--- App.js
+++ App.js
@@ -1,20 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import axios from 'axios';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Card from './Components/Card';
+import Compass from './Components/Compass';
 
-export default function App() {
+export default App = () => {
+  const [data, setData] = useState(''); 
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://172.17.218.107:8081/data'); 
+      setData(response.data); 
+    } catch (error) {
+      console.error("Error al conectar con ESP8266:", error.message);
+      if (error.response) {
+        console.error("Respuesta del servidor:", error.response.data);
+        console.error("Código de estado:", error.response.status);
+      } else if (error.request) {
+        console.error("No se recibió respuesta:", error.request);
+      }
+    }
+    
+  };
+
+  useEffect(() => {
+    fetchData(); 
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header text="Dirección del viento" style={styles.header} />
+      <Compass />
+      <Text style={styles.dataText}>
+        {data ? Datos del ESP8266: ${data} : "Conectando..."}
+      </Text>
+      <Footer text="Creadores y asesores:" style={styles.footer} />
     </View>
-  );
-}
+  );  
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#252525',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between', // Distribuye Header arriba y Footer abajo
+    paddingVertical: 20, // Espaciado adicional en la parte superior e inferior
+  },
+  header: {
+    marginTop: 10, // Ajusta según el espacio deseado arriba
+  },
+  footer: {
+    marginBottom: 10, // Ajusta según el espacio deseado abajo
+  },
+  dataText: {
+    color: '#fff',
+    fontSize: 16,
+    marginTop: 20,
   },
 });
