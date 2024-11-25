f01ef4ffb63e9bdee01a970206dcf13e24ee1613
GerryRguez <gfarfan_22@alu.uabcs.mx>
2024-11-24 18:27:28 -0700

    Updated functionality in Compass.js

--- Components/Compass.js
+++ Components/Compass.js
@@ -1,33 +1,24 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, StyleSheet } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
 
-const Compass = ({code}) => {
-const [directionCode, setDirectionCode] = useState('');
-const [rotation, setRotation] = useState(90);
+export default Compass = ({directionCode}) => {
 
-const directionMap = {
-    '000': 0,    
-    '001': 45,   
-    '010': 90,   
-    '011': 135,  
-    '100': 180,  
-    '101': 225,  
-    '110': 270,  
-    '111': 315,  
-};
-
-const handleDirectionChange = (code) => {
-    setDirectionCode(code);
-    
-    if (directionMap.hasOwnProperty(code)) {
-      setRotation();
-    } else {
-      setRotation(0);
-    }
+  const directionMap = {
+    "000": 0, // N
+    "001": 45, // NE
+    "010": 90, // E
+    "011": 135, // SE
+    100: 180, // S
+    101: 225, // SW
+    110: 270, // W
+    111: 315, // NW
   };
+
+  const rotation = directionMap[directionCode] || 0;
 
   return (
     <View style={styles.compassContainer}>
+
       <Text style={[styles.directionText, styles.northText]}>N</Text>
       <Text style={[styles.directionText, styles.southText]}>S</Text>
       <Text style={[styles.directionText, styles.eastText]}>E</Text>
@@ -37,73 +28,56 @@ const handleDirectionChange = (code) => {
       <Text style={[styles.directionText, styles.southeastText]}>SE</Text>
       <Text style={[styles.directionText, styles.southwestText]}>SW</Text>
 
-      <View style={[styles.needleContainer, { transform: [{ rotate: `${directionMap[code]}deg` }] }]}>
+      <View
+        style={[
+          styles.needleContainer,
+          { transform: [{ rotate: "${rotation}deg" }] }
+        ]}
+      >
         <View style={styles.needle} />
       </View>
-
-      {/* <TextInput
-        style={styles.input}
-        placeholder="Ingresa el código binario"
-        value={directionCode}
-        onChangeText={handleDirectionChange}
-        keyboardType="numeric"
-        maxLength={3}
-      /> */}
     </View>
   );
 };
-
-export default Compass;
 
 const styles = StyleSheet.create({
   compassContainer: {
     width: 250,
     height: 250,
     borderRadius: 125,
-    backgroundColor: '#1e2b38',
-    justifyContent: 'center',
-    alignItems: 'center',
-    position: 'relative',
+    backgroundColor: "#1e2b38",
+    justifyContent: "center",
+    alignItems: "center",
+    position: "relative",
     borderWidth: 2,
-    borderColor: '#888',
+    borderColor: "#888",
   },
   directionText: {
     fontSize: 20,
-    fontWeight: 'bold',
-    color: '#fff',
-    position: 'absolute',
+    fontWeight: "bold",
+    color: "#fff",
+    position: "absolute",
   },
-  northText: { top: 10, alignSelf: 'center' },
-  southText: { bottom: 10, alignSelf: 'center' },
-  eastText: { right: 10, alignSelf: 'center' },
-  westText: { left: 10, alignSelf: 'center' },
+  northText: { top: 10, alignSelf: "center" },
+  southText: { bottom: 10, alignSelf: "center" },
+  eastText: { right: 10, alignSelf: "center" },
+  westText: { left: 10, alignSelf: "center" },
   northeastText: { top: 40, right: 40 },
   northwestText: { top: 40, left: 40 },
   southeastText: { bottom: 40, right: 40 },
   southwestText: { bottom: 40, left: 40 },
   needleContainer: {
-    position: 'absolute',
+    position: "absolute",
     width: 10,
-    height: '70%',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
+    height: "70%",
+    justifyContent: "flex-start",
+    alignItems: "center",
   },
   needle: {
     width: 4,
-    height: '70%',
-    backgroundColor: 'red',
-    position: 'absolute',
+    height: "70%",
+    backgroundColor: "red",
+    position: "absolute",
     top: 0,
-  },
-  input: {
-    position: 'absolute',
-    bottom: -50,
-    width: 200,
-    padding: 10,
-    borderWidth: 1,
-    borderColor: '#888',
-    backgroundColor: '#fff',
-    borderRadius: 5,
-    textAlign: 'center',
   },
 });
